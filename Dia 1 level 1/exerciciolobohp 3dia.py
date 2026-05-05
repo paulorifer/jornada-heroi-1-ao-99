18d153cd1c8343dc992dfdf16ada54bb33ba15e6
Paulo <paulorifer@gmail.com>
2026-05-05 20:01:45 -0300

    Dia 3 - fixacao variaveis e condicoes

---
+++ Dia 1 level 1/exerciciolobohp 3dia.py
@@ -0,0 +1,10 @@
+nome = "lobo"
+adagalad = 25
+hplobo = 60
+ataqlobo = hplobo - adagalad * 2
+
+
+if ataqlobo >= 0:
+    print(f"{nome} Sofreu ataque . Resta de vida {ataqlobo}")
+else:
+    print(f"{nome} Sofreu a ataque e não resistiu {ataqlobo}")
